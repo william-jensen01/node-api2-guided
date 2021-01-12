c246110d7b849cbd123f866cfcb7a4f3d8b3ad5a
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-12 19:22:56 +0100

    wip

--- api/dogs/dogs-router.js
+++ api/dogs/dogs-router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const Dog = require('./dogs-model');
+const router = express.Router();
 
 // DOGS ENDPOINTS
 // DOGS ENDPOINTS
 // DOGS ENDPOINTS
-router.get('/api/dogs', (req, res) => {
+router.get('/', (req, res) => {
   Dog.find()
     .then(dogs => {
       res.status(200).json(dogs);
