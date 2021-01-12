06b03d8a4dc580de7798e277da5790c390a16e18
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-12 18:42:33 +0100

    wip

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const server = require('./api/server.js')
+const server = require('./api/server.js');
 
-// we take this out
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
