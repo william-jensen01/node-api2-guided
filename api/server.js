2e188fbd8a0cd2d6026874b08665767bebe5eaaa
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-12 19:19:56 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -4,24 +4,7 @@ const server = express();
 
 server.use(express.json());
 server.use('/api/adopters', adopterRouter)
-
-const Dog = require('./dogs/dogs-model');
-
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-server.get('/api/dogs', (req, res) => {
-  Dog.find()
-    .then(dogs => {
-      res.status(200).json(dogs);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the dogs',
-      });
-    });
-});
+server.use('/')
 
 // OTHER ENDPOINTS
 // OTHER ENDPOINTS
